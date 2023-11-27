5898cf3da63a251bb04a2da349c538f540d6a4e6
Tim Maes <maestim@cronos.be>
2023-11-27 15:46:27 +0100

    Tidy up

--- src/GraphR.API/ServiceCollectionExtensions.cs
+++ src/GraphR.API/ServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
 using GrapR.Application;
-using GrapR.Core;
 using GrapR.Infrastructure;
 
 namespace GrapR.WebAPI;
@@ -9,7 +8,6 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddSolutionComponents(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddApplication();
-        services.AddCore();
         services.AddInfrastructure(configuration);
 
         return services;
