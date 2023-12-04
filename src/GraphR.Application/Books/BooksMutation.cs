a7f2d1488230d63f3989dd4b91b78a961f31e317
Tim Maes <tim.maes91@gmail.com>
2023-12-04 10:31:42 +0100

    Rename input

--- src/GraphR.Application/Books/BooksMutation.cs
+++ src/GraphR.Application/Books/BooksMutation.cs
@@ -6,6 +6,6 @@ namespace GraphR.Application.Books;
 
 public sealed class BooksMutation
 {
-    public async Task<int> CreateBook([Service] ICreateBookMutationHandler handler, CreateBookParameters request)
-        => await handler.Handle(request);
+    public async Task<int> CreateBook([Service] ICreateBookMutationHandler handler, CreateBookParameters parameters)
+        => await handler.Handle(parameters);
 }
